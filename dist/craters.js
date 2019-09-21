4b7b56b6fc4a394f3a425aaaeb854ef76d09b369
swashvirus <johnswana49@gmail.com>
2019-09-21 05:42:46 +0200

    i've added unminified  version two builds

---
+++ dist/craters.js
@@ -0,0 +1,459 @@
+class game {
+  constructor (game, width, height, frames, debug) {
+    this.game = game || 'body';
+    this.constants = {
+      gravity: {
+        x: 0,
+        y: 100
+      },
+
+      width: width,
+      height: height,
+      frames: frames,
+      debug: debug,
+      bgcolor: 'rgba(0,0,0,0)',
+      color: '#ff0',
+      font: '1em Arial'
+    };
+
+    this.state = {
+      entities: []
+    };
+
+    // Generate a canvas and store it as our viewport
+    var canvas = document.createElement('canvas');
+    var context = canvas.getContext('2d');
+    // Pass our canvas' context to our getPixelRatio method
+    var backingStores = ['webkitBackingStorePixelRatio', 'mozBackingStorePixelRatio', 'msBackingStorePixelRatio', 'oBackingStorePixelRatio', 'backingStorePixelRatio'];
+    var deviceRatio = window.devicePixelRatio;
+    // Iterate through our backing store props and determine the proper backing ratio.
+    var backingRatio = backingStores.reduce(function (prev, curr) {
+      return (context.hasOwnProperty(curr) ? context[curr] : 1)
+    });
+    // Return the proper pixel ratio by dividing the device ratio by the backing ratio
+    var ratio = deviceRatio / backingRatio;
+
+    // Set the canvas' width then downscale via CSS
+    canvas.width = Math.round(this.constants.width * ratio);
+    canvas.height = Math.round(this.constants.height * ratio);
+    canvas.style.width = this.constants.width + 'px';
+    canvas.style.height = this.constants.height + 'px';
+    // Scale the context so we get accurate pixel density
+    context.setTransform(ratio, 0, 0, ratio, 0, 0);
+
+    this.viewport = canvas;
+    this.viewport.id = 'gameViewport';
+
+    // Get and store the canvas context as a global
+    this.context = this.viewport.getContext('2d');
+
+    // Append viewport into our game within the dom
+    this.game = document.querySelector(this.game);
+    this.game.insertBefore(this.viewport, this.game.firstChild);
+
+    // Initiate core modules with the current scope
+    this.loop = new loop(this);
+    this.intitiate();
+  }
+
+  intitiate () {
+
+  }
+
+  update (scope, now) {
+    var state = scope.state || [];
+    var entities = state.entities;
+    for (var entity = 0; entity < entities.length; entity++) {
+      // Fire off each active entities `render` method
+      entities[entity].update();
+    }
+  }
+
+  render (scope, now) {
+    // Setup globals
+    var w = scope.constants.width;
+    var h = scope.constants.height;
+
+    // Clear out the canvas
+    scope.context.font = scope.constants.font;
+    scope.context.save();
+    scope.context.clearRect(0, 0, w, h);
+    scope.context.fillStyle = scope.constants.bgcolor;
+    scope.context.fillRect(0, 0, w, h);
+    scope.context.fill();
+    scope.context.restore();
+    // Spit out some text
+    scope.context.fillStyle = scope.constants.color;
+    // If we want to show the FPS, then render it in the top right corner.
+    if (scope.constants.debug) {
+      scope.context.fillText('fps : ' + scope.loop.fps, w - 100, 50);
+    }
+
+    // If there are entities, iterate through them and call their `render` methods
+    var state = scope.state || [];
+    var entities = state.entities;
+    for (var entity = 0; entity < entities.length; entity++) {
+      // Fire off each active entities `render` method
+      entities[entity].render();
+    }
+  }
+}
+class loop {
+  constructor (scope) {
+    return this.loop(scope)
+  }
+
+  loop (scope) {
+    var loop = {};
+    // Initialize timer variables so we can calculate FPS
+    var fps = scope.constants.frames;
+    var fpsInterval = 1000 / fps;
+    var before = window.performance.now();
+    // Set up an object to contain our alternating FPS calculations
+    var cycles = {
+      new: {
+        frameCount: 0,
+        startTime: before,
+        sinceStart: 0
+      },
+      old: {
+        frameCount: 0,
+        startTime: before,
+        sineStart: 0
+      }
+    };
+
+    // Alternating Frame Rate vars
+    var resetInterval = 5;
+    var resetState = 'new';
+
+    loop.fps = 0;
+
+    // Main game rendering loop
+    loop.main = function mainLoop (tframe) {
+      // Request a new Animation Frame
+      // setting to `stopLoop` so animation can be stopped via
+      // `window.cancelAnimationFrame( loop.stopLoop )`
+      loop.stopLoop = window.requestAnimationFrame(loop.main);
+
+      // How long ago since last loop?
+      var now = tframe;
+      var elapsed = now - before;
+      var activeCycle;
+      var targetResetInterval;
+
+      // If it's been at least our desired interval, render
+      if (elapsed > fpsInterval) {
+        // Set before = now for next frame, also adjust for
+        // specified fpsInterval not being a multiple of rAF's interval (16.7ms)
+        // ( http://stackoverflow.com/a/19772220 )
+        before = now - (elapsed % fpsInterval);
+
+        // Increment the vals for both the active and the alternate FPS calculations
+        for (var calc in cycles) {
+          ++cycles[calc].frameCount;
+          cycles[calc].sinceStart = now - cycles[calc].startTime;
+        }
+
+        // Choose the correct FPS calculation, then update the exposed fps value
+        activeCycle = cycles[resetState];
+        loop.fps = Math.round(1000 / (activeCycle.sinceStart / activeCycle.frameCount) * 100) / 100;
+
+        // If our frame counts are equal....
+        targetResetInterval = (cycles.new.frameCount === cycles.old.frameCount
+          ? resetInterval * fps // Wait our interval
+          : (resetInterval * 2) * fps); // Wait double our interval
+
+        // If the active calculation goes over our specified interval,
+        // reset it to 0 and flag our alternate calculation to be active
+        // for the next series of animations.
+        if (activeCycle.frameCount > targetResetInterval) {
+          cycles[resetState].frameCount = 0;
+          cycles[resetState].startTime = now;
+          cycles[resetState].sinceStart = 0;
+
+          resetState = (resetState === 'new' ? 'old' : 'new');
+        }
+
+        // Update the game state
+        scope.update(scope, now);
+        // Render the next frame
+        scope.render(scope, now);
+      }
+    };
+
+    // Start off main loop
+    loop.main();
+
+    return loop
+  }
+}
+
+class entity {
+  constructor () {
+    // Setup the defaults if no parameters are given
+    // Type represents the collision detector's handling
+    this.type = this.type || 'dynamic';
+
+    // Collision represents the type of collision
+    // another object will receive upon colliding
+    this.collision = this.collision || 'elastic';
+    this.state = {
+
+      size: {
+        x: 10,
+        y: 10
+      },
+      pos: {
+        x: 0,
+        y: 0
+      },
+      vel: {
+        x: 0,
+        y: 0
+      },
+      accel: {
+        x: 0,
+        y: 0
+      },
+      radius: 10,
+      angle: 0
+    };
+
+    this.entities = [];
+  }
+
+  update () {
+    // update the sub entities if there's any
+    // by firing off the update function one by one
+    for (var entity = 0; entity < this.entities.length; entity++) {
+      this.entities[entity].update();
+    }
+
+    switch (this.type) {
+      case 'dynamic':
+        this.state.vel.x += this.state.accel.x;
+        this.state.vel.y += this.state.accel.y;
+        this.state.pos.x += this.state.vel.x;
+        this.state.pos.y += this.state.vel.y;
+        break
+      case 'kinematic':
+        this.state.vel.x += this.state.accel.x;
+        this.state.vel.y += this.state.accel.y;
+        this.state.pos.x += this.state.vel.x;
+        this.state.pos.y += this.state.vel.y;
+        break
+    }
+  }
+
+  render () {
+    for (var entity = 0; entity < this.entities.length; entity++) {
+      this.entities[entity].render();
+    }
+  }
+}
+
+class sprite extends entity {
+  constructor (scope, args) {
+    super();
+
+    this.scope = scope;
+    this.state = {
+      cord: args.pos || {
+        x: 0,
+        y: 0
+      },
+      pos: {
+        x: 0,
+        y: 0
+      },
+      size: args.size || {
+        x: 0,
+        y: 0
+      },
+      frames: args.frames || [],
+      angle: args.angle || 0,
+      image: args.image || new Image(),
+      delay: args.delay || 5,
+      tick: args.tick || 0,
+      orientation: args.orientation || 'horizontal'
+    };
+  }
+
+  update () {
+    if (this.state.tick <= 0) {
+      if (this.orientation == 'vertical') {
+        this.state.pos.y = this.state.frames.shift();
+        this.state.frames.push(this.state.pos.y);
+      } else {
+        this.state.pos.x = this.state.frames.shift();
+        this.state.frames.push(this.state.pos.x);
+      }
+
+      this.state.tick = this.state.delay;
+    }
+
+    this.state.tick--;
+  }
+
+  render () {
+    super.render(this);
+
+    this.scope.context.save();
+    this.scope.context.translate(this.state.pos.x + (this.state.size.x / 2), this.state.pos.y + (this.state.size.y / 2));
+    this.scope.context.rotate((this.state.angle) * (Math.PI / 180));
+    this.scope.context.translate(-(this.state.pos.x + (this.state.size.x / 2)), -(this.state.pos.y + (this.state.size.y / 2)));
+
+    this.scope.context.drawImage(this.state.image,
+      (this.state.pos.x * this.state.size.x), (this.state.pos.y * this.state.size.y), this.state.size.x, this.state.size.y,
+      this.state.cord.x, this.state.cord.y, this.state.size.x, this.state.size.y);
+
+    this.scope.context.restore();
+  }
+}
+
+class loader {
+  constructor () {
+    this.rescache = {};
+  }
+
+  load (res, cbk) {
+    var that = this;
+    if (res instanceof Array) {
+      res.forEach(function (url) {
+        that.rescache[url] = false;
+        that.fetch(url, cbk);
+      });
+    } else {
+      that.rescache[url] = false;
+      that.fetch(res, cbk);
+    }
+  }
+
+  fetch (url, cbk) {
+    var that = this;
+    if (that.rescache[url]) {
+      return that.rescache[url]
+    } else {
+      var img = new Image();
+      img.onload = function () {
+        that.rescache[url] = img;
+        that.ready(cbk);
+      };
+
+      img.src = url;
+    }
+  }
+
+  ready (cbk) {
+    var that = this;
+    if (typeof cbk === 'function') {
+      var ready = true;
+      for (var item in that.rescache) {
+        if (that.rescache.hasOwnProperty(item) && !that.rescache[item]) {
+          ready = false;
+        }
+      }
+
+      if (ready) cbk();
+    }
+  }
+}
+
+// modified soundbox.js lib
+class sound {
+  constructor () {
+    this.sounds = {}; // The loaded sounds and their instances
+    this.instances = []; // Sounds that are currently playing
+    this.default_volume = 1;
+  }
+
+  load (sound_name, path, callback) {
+    this.sounds[sound_name] = new Audio(path);
+    if (typeof callback === 'function') {
+      this.sounds[sound_name].addEventListener('canplaythrough', callback);
+    } else {
+      return new Promise((resolve, reject) => {
+        this.sounds[sound_name].addEventListener('canplaythrough', resolve);
+        this.sounds[sound_name].addEventListener('error', reject);
+      })
+    }
+  };
+
+  remove (sound_name) {
+    if (typeof this.sounds !== 'undefined') {
+      delete this.sounds[sound_name];
+    }
+  };
+
+  unlock (sound_name, callback, volume, loop) {
+    var that = this;
+    var events = ['touchstart', 'touchend', 'mousedown', 'keydown'];
+    var unlock = function unlock () {
+      events.forEach(function (event) {
+        document.body.removeEventListener(event, unlock);
+      });
+      that.play(sound_name, callback, volume, loop);
+    };
+
+    events.forEach(function (event) {
+      document.body.addEventListener(event, unlock, false);
+    });
+  }
+
+  play (sound_name, callback, volume, loop) {
+    loop = loop || false;
+
+    if (typeof this.sounds[sound_name] === 'undefined') {
+      console.error("Can't find sound called '" + sound_name + "'.");
+      return false
+    }
+
+    var soundInstance = this.sounds[sound_name].cloneNode(true);
+    soundInstance.volume = typeof volume === 'number' ? volume : this.default_volume;
+    soundInstance.loop = loop;
+    soundInstance.play();
+    this.instances.push(soundInstance);
+
+    // Don't forget to remove the instance from the instances array
+    soundInstance.addEventListener('ended', () => {
+      var index = this.instances.indexOf(soundInstance);
+      if (index != -1) this.instances.splice(index, 1);
+    });
+
+    // Attach the callback / promise
+    if (typeof callback === 'function') {
+      soundInstance.addEventListener('ended', callback);
+      return true
+    }
+
+    return new Promise((resolve, reject) => soundInstance.addEventListener('ended', resolve))
+  };
+
+  stop_all () {
+    // Pause all currently playing sounds
+
+    // Shallow clone the array to avoid issues with instances auto-removing themselves
+    var instances_to_stop = this.instances.slice();
+    for (var instance of instances_to_stop) {
+      instance.pause();
+      instance.dispatchEvent(new Event('ended'));
+    }
+  }
+}
+
+// Craters.js micro game framework
+
+const Boundary = function numberBoundary (min, max) {
+  return Math.min(Math.max(this, min), max)
+};
+// Expose methods
+Number.prototype.boundary = Boundary;
+
+class craters {
+  static version () {
+    return '0.0.0.3'
+  }
+}
+
+export { craters, entity, game, loader, sound, sprite };
