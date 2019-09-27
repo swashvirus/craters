c91d14e3b8fe2cae888589c531f2dd4a1cc5868b
swashvirus <johnswana49@gmail.com>
2019-09-27 12:53:17 +0200

    brand new builds i've also updated the sample game

--- dist/craters.js
+++ dist/craters.js
@@ -1,98 +1,41 @@
-var cg = (function (exports) {
+var Craters = (function (exports) {
   'use strict';
 
-  class Game {
-    constructor (container, width, height, frames, debug) {
-      this.container = container || 'body';
-      this.state = {
-        width: width,
-        height: height,
-        frames: frames,
-        debug: debug,
-        bgcolor: 'rgba(0,0,0,0)',
-        color: '#ff0',
-        font: '1em Arial'
-      };
-
-      this.entities = [];
-      // Generate a canvas and store it as our viewport
-      var canvas = document.createElement('canvas');
-      var context = canvas.getContext('2d');
-      // Pass our canvas' context to our getPixelRatio method
-      var backingStores = ['webkitBackingStorePixelRatio', 'mozBackingStorePixelRatio', 'msBackingStorePixelRatio', 'oBackingStorePixelRatio', 'backingStorePixelRatio'];
-      var deviceRatio = window.devicePixelRatio;
-      // Iterate through our backing store props and determine the proper backing ratio.
-      var backingRatio = backingStores.reduce(function (prev, curr) {
-        return (Object.prototype.hasOwnProperty.call(context, curr) ? context[curr] : 1)
-      });
-      // Return the proper pixel ratio by dividing the device ratio by the backing ratio
-      var ratio = deviceRatio / backingRatio;
-
-      // Set the canvas' width then downscale via CSS
-      canvas.width = Math.round(this.state.width * ratio);
-      canvas.height = Math.round(this.state.height * ratio);
-      canvas.style.width = this.state.width + 'px';
-      canvas.style.height = this.state.height + 'px';
-      // Scale the context so we get accurate pixel density
-      context.setTransform(ratio, 0, 0, ratio, 0, 0);
-
-      this.viewport = canvas;
-      this.viewport.id = 'gameViewport';
-
-      // Get and store the canvas context as a global
-      this.context = this.viewport.getContext('2d');
-
-      // Append viewport into our game within the dom
-      this.container = document.querySelector(this.container);
-      this.container.insertBefore(this.viewport, this.container.firstChild);
-
-      // Initiate core modules with the current scope
-      this.loop = new Loop(this);
-      this.intitiate();
-    }
-
-    intitiate () {
-
-    }
-
-    update (scope, now) {
-      for (var entity = 0; entity < this.entities.length; entity++) {
-        // Fire off each active entities `render` method
-        this.entities[entity].update();
+  // Rect collision tests the edges of each rect to
+  // test whether the objects are overlapping the other
+  class Collision {
+  	static detector (collider, collidee) {
+  	    // Store the collider and collidee edges
+  	    var l1 = collider.state.pos.x;
+  	    var t1 = collider.state.pos.y;
+  	    var r1 = l1 + collider.state.size.x;
+  	    var b1 = t1 + collider.state.size.y;
+  	    
+  	    var l2 = collidee.state.pos.x;
+  	    var t2 = collidee.state.pos.y;
+  	    var r2 = l2 + collidee.state.size.x;
+  	    var b2 = t2 + collidee.state.size.y;
+  	    
+  	    // If the any of the edges are beyond any of the
+  	    // others, then we know that the box cannot be
+  	    // colliding
+  	    if (b1 < t2 || t1 > b2 || r1 < l2 || l1 > r2) {
+  	        return false;
+  	    }
+  	    
+  	    // If the algorithm made it here, it had to collide
+  	    return true;
       }
-    }
-
-    render (scope, now) {
-      // Setup globals
-      var w = scope.state.width;
-      var h = scope.state.height;
-
-      // Clear out the canvas
-      scope.context.font = scope.state.font;
-      scope.context.save();
-      scope.context.clearRect(0, 0, w, h);
-      scope.context.fillStyle = scope.state.bgcolor;
-      scope.context.fillRect(0, 0, w, h);
-      scope.context.fill();
-      scope.context.restore();
-      // Spit out some text
-      scope.context.fillStyle = scope.state.color;
-      // If we want to show the FPS, then render it in the top right corner.
-      if (scope.state.debug) {
-        scope.context.fillText('fps : ' + scope.loop.fps, w - 100, 50);
-      }
-      // If there are entities, iterate through them and call their `render` methods
-      for (var entity = 0; entity < this.entities.length; entity++) {
-        // Fire off each active entities `render` method
-        this.entities[entity].render();
-      }
-    }
   }
+
+  // Game Loop Module
+  // This module contains the game loop, which handles
+  // updating the game state and re-rendering the canvas
+  // (using the updated state) at the configured FPS.
   class Loop {
-    constructor (scope) {
+    constructor (scope, fps) {
       var loop = {};
       // Initialize timer variables so we can calculate FPS
-      var fps = scope.state.frames;
       var fpsInterval = 1000 / fps;
       var before = window.performance.now();
       // Set up an object to contain our alternating FPS calculations
@@ -170,13 +113,77 @@ var cg = (function (exports) {
 
       // Start off main loop
       loop.main();
-
       return loop
     }
   }
 
-  class Entity {
+  class Canvas {
+  	constructor (width, height, container) {
+  		var container = document.querySelector(container || 'body');
+  		// Generate a canvas and store it as our viewport
+  	    var canvas = document.createElement('canvas');
+  	    var context = canvas.getContext('2d');
+  	    // Pass our canvas' context to our getPixelRatio method
+  	    var backingStores = ['webkitBackingStorePixelRatio', 'mozBackingStorePixelRatio', 'msBackingStorePixelRatio', 'oBackingStorePixelRatio', 'backingStorePixelRatio'];
+  	    var deviceRatio = window.devicePixelRatio;
+  	    // Iterate through our backing store props and determine the proper backing ratio.
+  	    var backingRatio = backingStores.reduce(function (prev, curr) {
+  	      return (Object.prototype.hasOwnProperty.call(context, curr) ? context[curr] : 1)
+  	    });
+  	    // Return the proper pixel ratio by dividing the device ratio by the backing ratio
+  	    var ratio = deviceRatio / backingRatio;
+  	
+  	    // Set the canvas' width then downscale via CSS
+  	    canvas.width = Math.round(width * ratio);
+  	    canvas.height = Math.round(height * ratio);
+  	    canvas.style.width = width + 'px';
+  	    canvas.style.height = height + 'px';
+  	    // Scale the context so we get accurate pixel density
+  	    context.setTransform(ratio, 0, 0, ratio, 0, 0);
+  	    // Append viewport into our game within the dom
+  	    container.insertBefore(canvas, container.firstChild);
+  	    
+  	    return canvas // return the canvas
+  	}
+  }
+
+  class Game {
+    constructor (container, width, height, fps, debug) {
+      this.entities = [];
+      this.state = {
+        container: container,
+        size: {
+  	      x: 10,
+  	      y: 10
+        },
+        
+        bgcolor: 'rgba(0,0,0,0)',
+        color: '#ff0',
+        font: '1em Arial'
+      };
+    }
+
+    update () {
+      for (var entity = 0; entity < this.entities.length; entity++) {
+        // Fire off each active entities `render` method
+        this.entities[entity].update();
+      }
+    }
+    
+    render () {
+  	for (var entity = 0; entity < this.entities.length; entity++) {
+      // Fire off each active entities `render` method
+      this.entities[entity].render(); }
+    }
+    
+    clearContext (context, size) {
+  	  context.clearRect(0, 0, size.x, size.y);
+    }
+  }
+
+  class Entity extends Game {
     constructor () {
+      super();
       this.state = {
         size: {
           x: 10,
@@ -197,26 +204,15 @@ var cg = (function (exports) {
         radius: 10,
         angle: 0
       };
-
-      this.entities = [];
     }
 
     update () {
-      // update the sub entities if there's any
-      // by firing off the update function one by one
-      for (var entity = 0; entity < this.entities.length; entity++) {
-        this.entities[entity].update();
-      }
+      super.update ();
+      
       this.state.vel.x += this.state.accel.x;
       this.state.vel.y += this.state.accel.y;
       this.state.pos.x += this.state.vel.x;
       this.state.pos.y += this.state.vel.y;
-    }
-
-    render () {
-      for (var entity = 0; entity < this.entities.length; entity++) {
-        this.entities[entity].render();
-      }
     }
   }
 
@@ -225,29 +221,30 @@ var cg = (function (exports) {
       super();
 
       this.scope = scope;
-      this.state = {
-        pos: args.pos || {
+      this.state.pos = args.pos || {
           x: 0,
           y: 0
-        },
-        crop: {
+        };
+        this.state.crop = {
           x: 0,
           y: 0
-        },
-        size: args.size || {
+        };
+        this.state.size = args.size || {
           x: 0,
           y: 0
-        },
-        frames: args.frames || [],
-        angle: args.angle || 0,
-        image: args.image || new Image(),
-        delay: args.delay || 5,
-        tick: args.tick || 0,
-        orientation: args.orientation || 'horizontal'
-      };
-    }
+        };
+        
+        this.state.frames = args.frames || [];
+        this.state.angle = args.angle || 0;
+        this.state.image = args.image || new Image();
+        this.state.delay = args.delay || 5;
+        this.state.tick = args.tick || 0;
+        this.state.orientation = args.orientation || 'horizontal';
+      }
 
     update () {
+    super.update ();
+    
       if (this.state.tick <= 0) {
         if (this.orientation === 'vertical') {
           this.state.crop.y = this.state.frames.shift();
@@ -264,7 +261,7 @@ var cg = (function (exports) {
     }
 
     render () {
-      super.render(this);
+      super.render();
 
       this.scope.context.save();
       this.scope.context.translate(this.state.crop.x + (this.state.size.x / 2), this.state.crop.y + (this.state.size.y / 2));
@@ -417,14 +414,17 @@ var cg = (function (exports) {
 
   class Craters {
     static version () {
-      return '0.0.0.4'
+      return '0.0.0.5'
     }
   }
 
+  exports.Canvas = Canvas;
+  exports.Collision = Collision;
   exports.Craters = Craters;
   exports.Entity = Entity;
   exports.Game = Game;
   exports.Loader = Loader;
+  exports.Loop = Loop;
   exports.Sound = Sound;
   exports.Sprite = Sprite;
 
