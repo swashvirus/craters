cf13619873f688e3b4697115299f9e16d2c14d29
swashvirus <johnswana49@gmail.com>
2019-09-27 12:58:19 +0200

    i've updated the starter 'it's working game'

--- app/game.js
+++ app/game.js
@@ -1,12 +1,22 @@
 'use strict';
-import { Game } from './craters/craters.js'
+import { Game, Canvas, Loop } from './craters/craters.js'
 
 class mygame extends Game {
-
+	constructor (container, width, height){
+		super ();
+		
+		this.state.size = {x: width, y: height}
+		this.viewport = new Canvas(this.state.size.x, this.state.size.y, container);
+		this.context = this.viewport.getContext('2d')
+		this.loop = new Loop(this, 60)
+	}
+	
     render () {
-        super.render(this)
+        super.render()
+        this.clearContext(this.context, this.state.size)
+        
         this.context.font = '2em Arial'
-        this.context.fillText('It\'s working.️', 65, (this.state.height / 2), (this.state.width))
+        this.context.fillText('It\'s working.️', 65, (this.state.size.y / 2), (this.state.size.x))
     }
 }
 
