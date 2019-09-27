96a1cdf1800f6805d45fb28093859af95c87fe53
swashvirus <johnswana49@gmail.com>
2019-09-27 02:07:42 +0200

    game building in prlogress currently auto pilot mode # full of hacks

---
+++ examples/breakout-game/webpack.config.js
@@ -0,0 +1,12 @@
+const path = require('path')
+
+module.exports = {
+  entry: { './game': './game.js' },
+  output: {
+    path: path.resolve(__dirname, './'),
+    filename: '[name].min.js'
+  },
+  resolve: {
+	  modules: ['node_modules']
+  }
+}
